f6348ec24e2b3db252c6353cc04f44e1572c1474
FRANCIS AGBODJI <frank@digitalfrank.com>
2018-03-14 22:36:29 -0400

    added vanilla js image slideshow

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
         }
     });
 
+
     // ///////////////////////////// bottom tree animation scroll up & scroll down
     let tree = document.getElementById('tree');
 
@@ -54,6 +55,24 @@ $(document).ready(function(){
     
     lastScrollTop = st;
 }
+
+    // ///////////////////////////// slideshow animation
+
+    let current = 0,
+    slides = document.getElementsByClassName("slide");
+
+    setInterval(function() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+    }
+    current = (current != slides.length - 1) ? current + 1 : 0;
+    slides[current].style.opacity = 1;
+    }, 3000);
+
+
+
+
+    // ///////////////////////////// email lightbox pop up
 
 
 });
